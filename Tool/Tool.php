01b12f0bce567fb2043d8ae4966434d7922404de
6EKV1EIKXP43KPZ\Administrator <960337959@qq.com>
2017-12-29 15:31:28 +0800

    111111111111111111111111

--- Tool/Tool.php
+++ Tool/Tool.php
@@ -10,7 +10,19 @@
 namespace miikou\tool;
 
 
+/**
+ * 工具类
+ * Class Tool
+ * @package miikou\tool
+ */
 class Tool{
+
+    /**
+     * 获取动态IP地址
+     * $this->getIp();
+     * @param int $type
+     * @return mixed
+     */
     public function getIp($type = 0){
         $type       =  $type ? 1 : 0;
         static $ip  =   NULL;
@@ -33,5 +45,87 @@ class Tool{
         $long = sprintf("%u",ip2long($ip));
         $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
         return $ip[$type];
+    }
+
+    /**
+     * 自动生成验证码
+     * $this->create_code(1,'',6,'');
+     * @param $nums
+     * @param string $exist_array
+     * @param int $code_length
+     * @param string $prefix
+     * @return mixed
+     */
+    public function create_code($nums,$exist_array='',$code_length = 6,$prefix =''){
+        $characters = "0123456789";
+        $promotion_codes = array();//这个数组用来接收生成的优惠码
+        for($j = 0 ; $j < $nums; $j++) {
+            $code = '';
+            for ($i = 0; $i < $code_length; $i++) {
+                $code .= $characters[mt_rand(0, strlen($characters)-1)];
+            }
+            //如果生成的4位随机数不再我们定义的$promotion_codes数组里面
+            if( !in_array($code,$promotion_codes) ) {
+                if( is_array($exist_array) ) {
+                    if( !in_array($code,$exist_array) ) {//排除已经使用的优惠码
+                        $promotion_codes[$j] = strtoupper($prefix.$code); //将生成的新优惠码赋值给promotion_codes数组
+                    } else {
+                        $j--;
+                    }
+                } else {
+                    $promotion_codes[$j] = strtoupper($prefix.$code);//将优惠码赋值给数组
+                }
+            } else {
+                $j--;
+            }
+        }
+        return $promotion_codes[0];
+
+    }
+
+
+    /**
+     * 判断是否是手机端访问
+     * $this->is_mobile_request();
+     * @return bool
+     */
+    public function is_mobile_request(){
+        $_SERVER['ALL_HTTP'] = isset($_SERVER['ALL_HTTP']) ? $_SERVER['ALL_HTTP'] : '';
+        $mobile_browser = '0';
+        if(preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|iphone|ipad|ipod|android|xoom)/i', strtolower($_SERVER['HTTP_USER_AGENT'])))
+            $mobile_browser++;
+        if((isset($_SERVER['HTTP_ACCEPT'])) and (strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml') !== false))
+            $mobile_browser++;
+        if(isset($_SERVER['HTTP_X_WAP_PROFILE']))
+            $mobile_browser++;
+        if(isset($_SERVER['HTTP_PROFILE']))
+            $mobile_browser++;
+        $mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'],0,4));
+        $mobile_agents = array(
+            'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+            'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+            'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+            'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+            'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+            'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+            'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+            'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+            'wapr','webc','winw','winw','xda','xda-'
+        );
+        if(in_array($mobile_ua, $mobile_agents))
+            $mobile_browser++;
+        if(strpos(strtolower($_SERVER['ALL_HTTP']), 'operamini') !== false)
+            $mobile_browser++;
+        // Pre-final check to reset everything if the user is on Windows
+        if(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'windows') !== false)
+            $mobile_browser=0;
+        // But WP7 is also Windows, with a slightly different characteristic
+        if(strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'windows phone') !== false)
+            $mobile_browser++;
+        if($mobile_browser>0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
