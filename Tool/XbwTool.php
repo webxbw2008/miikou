53aba386f69ba45707cf2442581944dc67cd4220
6EKV1EIKXP43KPZ\Administrator <960337959@qq.com>
2017-12-29 13:49:35 +0800

    11111111111

---
+++ Tool/XbwTool.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * tpshop
+ * ============================================================================
+ * * 版权所有 2015-2027 深圳搜豹网络科技有限公司，并保留所有权利。
+ * 网站地址: http://www.tpshop.cn
+ * ----------------------------------------------------------------------------
+ * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和使用 .
+ * 不允许对程序代码以任何形式任何目的的再发布。
+ * 个人学习免费, 如果商业用途务必到TPshop官网购买授权.
+ * 采用TP5助手函数可实现单字母函数M D U等,也可db::name方式,可双向兼容
+ * ============================================================================
+ * $Author: IT宇宙人 2015-08-10 $
+ *
+ */
+
+namespace webxbw\tool;
+
+class XbwTool{
+    public function getIp($type = 0){
+        $type       =  $type ? 1 : 0;
+        static $ip  =   NULL;
+        if ($ip !== NULL) return $ip[$type];
+        if($_SERVER['HTTP_X_REAL_IP']){//nginx 代理模式下，获取客户端真实IP
+            $ip=$_SERVER['HTTP_X_REAL_IP'];
+        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {//客户端的ip
+            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+        }elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {//浏览当前页面的用户计算机的网关
+            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            $pos    =   array_search('unknown',$arr);
+            if(false !== $pos) unset($arr[$pos]);
+            $ip     =   trim($arr[0]);
+        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+            $ip     =   $_SERVER['REMOTE_ADDR'];//浏览当前页面的用户计算机的ip地址
+        }else{
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        // IP地址合法验证
+        $long = sprintf("%u",ip2long($ip));
+        $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+        return $ip[$type];
+    }
+}
