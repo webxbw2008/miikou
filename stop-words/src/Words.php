5dca3542b05ae3a08b922f8b85bc2604294487a1
6EKV1EIKXP43KPZ\Administrator <960337959@qq.com>
2018-01-03 10:44:44 +0800

    222222222222

---
+++ stop-words/src/Words.php
@@ -0,0 +1,41 @@
+<?php
+namespace miikou\stopwords\src;
+
+/**
+ * Class Words
+ * @package miikou\stopwords\src
+ */
+class Words
+{
+    protected $stop_words = array();
+
+    /**
+     * Create a new Words Instance
+     *
+     * @param mixed Optional words to set
+     */
+    public function __construct()
+    {
+        $words =explode("\r\n",file_get_contents('base_dic_full.txt'));
+        $this->stop_words = $words;
+    }
+
+    /**
+     * $a = new Words();
+        $bb = '我今天开着最牛公安上班';
+        $res = $a->mergeWords($bb);
+        var_dump($res);
+     *
+     * @param $key  原始字符串
+     * @param string $replace  替换为
+     * @return string
+     */
+    public function mergeWords($key,$replace='*'){
+        $badword1 =array_combine($this->stop_words,array_fill(0,count($this->stop_words),$replace));
+        $str = strtr($key,$badword1);
+        return $str;
+    }
+}
+
+
+
